533a7137f6a23a39153e72213643de62d5f47a53
jun951231 <jun951231@gmail.com>
2021-07-19 17:50:04 +0900

    July 19

--- oop/calculator.py
+++ oop/calculator.py
@@ -19,19 +19,33 @@ class Calculator(object):
     @staticmethod
     def main():
         while 1:
-            num1 = int(input('first number:'))
-            num2 = int(input('second number'))
-            calc = Calculator(num1, num2)
+
             menu = input('0-Exit 1: + 2: - 3: * 4: / \n')
             if menu == '0':
                 return
             elif menu == '1':
-
+                num1 = int(input('first number:'))
+                num2 = int(input('second number'))
+                calc = Calculator(num1, num2)
                 print(f'{calc.num1} + {calc.num2} = {calc.add()}')
+                break
+            elif menu == '1':
+                num1 = int(input('first number:'))
+                num2 = int(input('second number'))
+                calc = Calculator(num1, num2)
                 print(f'{calc.num1} - {calc.num2} = {calc.subtract()}')
+                break
+            elif menu == '1':
+                num1 = int(input('first number:'))
+                num2 = int(input('second number'))
+                calc = Calculator(num1, num2)
                 print(f'{calc.num1} * {calc.num2} = {calc.multiple()}')
+                break
+            elif menu == '1':
+                num1 = int(input('first number:'))
+                num2 = int(input('second number'))
+                calc = Calculator(num1, num2)
                 print(f'{calc.num1} / {calc.num2} = {calc.subtract()}')
-
                 break
             else:
                 print('Wrong Selected Number')
