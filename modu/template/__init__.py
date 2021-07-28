b82b13c02d0a6bb314f96ae37a839270086d92d5
jun951231 <jun951231@gmail.com>
2021-07-28 17:52:19 +0900

    July 28

--- modu/template/__init__.py
+++ modu/template/__init__.py
@@ -2,6 +2,7 @@ from common.menu import print_menu
 from modu.template.basic_plot import plot_show, plot_two_list_show, scatter
 from modu.template.changed_temperature_on_my_birthday import ChangedTemperatureOnMyBirthday
 
+
 if __name__ == '__main__':
     while 1:
         menu = print_menu(['exit', 'plot_show', 'plot_two_list_show', 'Scatter', 'Blank',
--- modu/template/pop_structure_visual.py
+++ modu/template/pop_structure_visual.py
@@ -1,10 +1,13 @@
-import csv
 import matplotlib.pyplot as plt
-
+import csv
+import numpy as np
+from matplotlib import font_manager, rc
+rc('font', family = font_manager.FontProperties(fname='C:\Windows\Fonts\Arial.ttf').get_name())
 
 class Population(object):
 
     data: [] = list()
+    home: [] = list()
 
     def read_data(self):
         data = csv.reader(open('./data/202106_202106_연령별인구현황_월간.csv', 'rt', encoding='UTF-8'))
@@ -12,21 +15,32 @@ class Population(object):
         # print([i for i in data])
         self.data = data
 
-    def pop_per_doing(self, dong: str) -> []:
+    def pop_per_dong(self, dong: str) -> []:
+        self.read_data()
         # print([i for i in self.data])
         arr = []
-        [arr.append(int(j)) for i in self.data if dong in i[0] for j in i[3:]]
+        [arr.append(j) for i in self.data if dong in i[0] for j in i[3:]]
         print('*' * 100)
         print([i for i in arr])
         return arr
-
-    def show_plot(self, arr: []):
+    '''
+    def pop_comparison(self, dong: str) -> []:
+        self.read_data()
+        np = []
+        dong = input('신도림동 :')
+        [np.array(i[3:], dtype=int)/int(i[2]) for i in self.data if dong in i[0] for i in self.data]
+        print([i for i in dong])
+        return np
+        '''
+    def show_plot(self, arr: [], dong: str):
+        self.read_data()
         plt.style.use('ggplot')
         plt.plot(arr)
         plt.show()
 
 
+
 if __name__ == '__main__':
     pop = Population()
     pop.read_data()
-    pop.show_plot(pop.pop_per_doing('역삼2동'))
+    pop.show_plot(pop.pop_per_dong('신도림동'))
